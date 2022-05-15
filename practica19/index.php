290758f02191b365c703a0bca2d2c8d567535221
Mariela Rocha <marirocodev@gmail.com>
2022-05-15 15:13:59 -0500

    examen2p
    Quickgigs, página de trabajos temporales

---
+++ practica19/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DLS EATS</title>
+</head>
+<body>
+    <h1>DLS EATS</h1>
+    <?php
+    ini_set('display_errors',1);
+    ini_set('display_startup_errors',1);
+    error_reporting(E_ALL);
+    include('conexion/index.php')
+     ?>
+    
+</body>
+</html>
